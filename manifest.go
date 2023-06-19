3f32102f5f8d2a10a0549df67fb5d846fffa1eac
kitstack <19929065+kitstack@users.noreply.github.com>
2023-06-18 22:10:39 -0400

    Version main: automatic version (commit: e593bb0)

--- manifest.go
+++ manifest.go
@@ -2,5 +2,5 @@ package eventkit
 
 var packageName = "eventkit"
 var packageVersion = "main"
-var packageCommit = "96993ba"
-var packageDate = "2023-06-19T01:51:49Z"
+var packageCommit = "e593bb0"
+var packageDate = "2023-06-19T02:10:39Z"
