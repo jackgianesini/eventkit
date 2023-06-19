d9e37a1ddebf30f924fbcb861658111429fe9479
kitstack <19929065+kitstack@users.noreply.github.com>
2023-06-18 22:24:14 -0400

    Version v1.0.2: automatic version (commit: e94f7a6)

--- manifest.go
+++ manifest.go
@@ -1,6 +1,6 @@
 package eventkit
 
 var packageName = "eventkit"
-var packageVersion = "main"
-var packageCommit = "9140b68"
-var packageDate = "2023-06-19T02:23:24Z"
+var packageVersion = "v1.0.2"
+var packageCommit = "e94f7a6"
+var packageDate = "2023-06-19T02:24:14Z"
